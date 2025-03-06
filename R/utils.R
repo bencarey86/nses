0f3dfb58f2396d5d8b740a726a6f9c8774192ae9
Ben Carey <george.carey@yale.edu>
2025-03-06 00:33:32 -0500

    add additional globalVariables

--- R/utils.R
+++ R/utils.R
@@ -3,5 +3,16 @@ utils::globalVariables(c(
     "x2nd_grade", "x3rd_grade", "x4th_grade", "x5th_grade", "x6th_grade", "x7th_grade",
     "x8th_grade", "x9th_grade", "x10th_grade", "x11th_grade", "x12th_grade_no_diploma",
     "total_educational_attainment_for_the_population_25_years_and_over", "bachelors_degree",
-    "masters_degree", "professional_school_degree", "doctorate_degree"
+    "masters_degree", "professional_school_degree", "doctorate_degree",
+    "in_labor_force_civilian_labor_force_unemployed", "in_labor_force",
+    "income_in_the_past_12_months_below_poverty_level",
+    "total_poverty_status_of_individuals_in_the_past_12_months_by_living_arrangement",
+    "median_housing_value", "tract_median_household_income",
+    "median_household_income",
+    "total_poverty_status_of_individuals_in_the_past_12_months_by_living_arrangement",
+    "percent_poverty", "tract_poverty", "total_tenure_by_occupants_per_room", "tract_density",
+    "occupants_per_room", "high_education", "low_education", "nses", "percent_crowded",
+    "percent_high_education", "percent_low_education", "percent_unemployed",
+    "standardized_median_housing_value", "standardized_median_income", "state",
+    "tract_high", "tract_low", "tract_median_housing_value"
 ))
